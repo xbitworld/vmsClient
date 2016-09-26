5c68bec5c62ad381f603591d168baa8e4682d84e
杜涛 <thomasdutao@hotmail.com>
2016-09-26 19:28:23 +0800

    基本修改完毕，有待完善图标到TABPage

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -40,7 +40,7 @@
             this.label4 = new System.Windows.Forms.Label();
             this.btnLogin = new System.Windows.Forms.Button();
             this.lstCamList = new System.Windows.Forms.ListView();
-            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.TabImageList = new System.Windows.Forms.ImageList(this.components);
             this.VideoPlayTab = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
@@ -74,7 +74,6 @@
             this.label12 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label11 = new System.Windows.Forms.Label();
-            this.imageList2 = new System.Windows.Forms.ImageList(this.components);
             this.VideoPlayTab.SuspendLayout();
             this.LoginGroup.SuspendLayout();
             this.DevGroup.SuspendLayout();
@@ -168,18 +167,18 @@
             this.lstCamList.Location = new System.Drawing.Point(12, 382);
             this.lstCamList.Name = "lstCamList";
             this.lstCamList.Size = new System.Drawing.Size(285, 193);
-            this.lstCamList.SmallImageList = this.imageList1;
+            this.lstCamList.SmallImageList = this.TabImageList;
             this.lstCamList.TabIndex = 4;
             this.lstCamList.UseCompatibleStateImageBehavior = false;
             this.lstCamList.View = System.Windows.Forms.View.List;
             this.lstCamList.SelectedIndexChanged += new System.EventHandler(this.lstCamList_SelectedIndexChanged);
             this.lstCamList.DoubleClick += new System.EventHandler(this.DBClickCamera);
             // 
-            // imageList1
+            // TabImageList
             // 
-            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
-            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
-            this.imageList1.Images.SetKeyName(0, "camera.png");
+            this.TabImageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("TabImageList.ImageStream")));
+            this.TabImageList.TransparentColor = System.Drawing.Color.Transparent;
+            this.TabImageList.Images.SetKeyName(0, "CamIcon.png");
             // 
             // VideoPlayTab
             // 
@@ -192,6 +191,7 @@
             this.VideoPlayTab.Controls.Add(this.tabPage4);
             this.VideoPlayTab.Controls.Add(this.tabPage5);
             this.VideoPlayTab.Controls.Add(this.tabPage6);
+            this.VideoPlayTab.ImageList = this.TabImageList;
             this.VideoPlayTab.Location = new System.Drawing.Point(304, 10);
             this.VideoPlayTab.Name = "VideoPlayTab";
             this.VideoPlayTab.SelectedIndex = 0;
@@ -200,58 +200,59 @@
             // 
             // tabPage1
             // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.ImageIndex = 0;
+            this.tabPage1.Location = new System.Drawing.Point(4, 31);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(750, 543);
+            this.tabPage1.Size = new System.Drawing.Size(750, 534);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "实时视频";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // tabPage2
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Location = new System.Drawing.Point(4, 31);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(750, 543);
+            this.tabPage2.Size = new System.Drawing.Size(750, 534);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "录像回放";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // tabPage3
             // 
-            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Location = new System.Drawing.Point(4, 31);
             this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Size = new System.Drawing.Size(750, 543);
+            this.tabPage3.Size = new System.Drawing.Size(750, 534);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "预录像播放";
             this.tabPage3.UseVisualStyleBackColor = true;
             // 
             // tabPage4
             // 
-            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Location = new System.Drawing.Point(4, 31);
             this.tabPage4.Name = "tabPage4";
             this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage4.Size = new System.Drawing.Size(750, 543);
+            this.tabPage4.Size = new System.Drawing.Size(750, 534);
             this.tabPage4.TabIndex = 3;
             this.tabPage4.Text = "按时间回放录像";
             this.tabPage4.UseVisualStyleBackColor = true;
             // 
             // tabPage5
             // 
-            this.tabPage5.Location = new System.Drawing.Point(4, 22);
+            this.tabPage5.Location = new System.Drawing.Point(4, 31);
             this.tabPage5.Margin = new System.Windows.Forms.Padding(2);
             this.tabPage5.Name = "tabPage5";
-            this.tabPage5.Size = new System.Drawing.Size(750, 543);
+            this.tabPage5.Size = new System.Drawing.Size(750, 534);
             this.tabPage5.TabIndex = 4;
             this.tabPage5.Text = "图片预览";
             this.tabPage5.UseVisualStyleBackColor = true;
             // 
             // tabPage6
             // 
-            this.tabPage6.Location = new System.Drawing.Point(4, 22);
+            this.tabPage6.Location = new System.Drawing.Point(4, 31);
             this.tabPage6.Name = "tabPage6";
-            this.tabPage6.Size = new System.Drawing.Size(750, 543);
+            this.tabPage6.Size = new System.Drawing.Size(750, 534);
             this.tabPage6.TabIndex = 5;
             this.tabPage6.Text = "数据库编辑";
             this.tabPage6.UseVisualStyleBackColor = true;
@@ -544,12 +545,6 @@
             this.label11.TabIndex = 2;
             this.label11.Text = "间隔(ms)：";
             // 
-            // imageList2
-            // 
-            this.imageList2.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
-            this.imageList2.ImageSize = new System.Drawing.Size(16, 16);
-            this.imageList2.TransparentColor = System.Drawing.Color.Transparent;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
@@ -590,7 +585,7 @@
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.TabPage tabPage3;
-        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ImageList TabImageList;
         private System.Windows.Forms.TabPage tabPage4;
         private System.Windows.Forms.GroupBox LoginGroup;
         private System.Windows.Forms.Label label6;
@@ -606,7 +601,6 @@
         private System.Windows.Forms.Button AutoCapBT;
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.PictureBox colorBox;
-        private System.Windows.Forms.ImageList imageList2;
         private System.Windows.Forms.TabPage tabPage5;
         private System.Windows.Forms.Button rstCAP;
         private System.Windows.Forms.TextBox IntervalTimeBox;
